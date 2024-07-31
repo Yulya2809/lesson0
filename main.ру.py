7dc13799a16404068779014f1337b00224915101
Yulya2809 <93946203+Yulya2809@users.noreply.github.com>
2024-07-31 20:52:00 +0300

    Add files via upload

---
+++ main.ру.py
@@ -0,0 +1,14 @@
+# "1st program"
+print('9**0.5*5')
+print(9**0.5*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(1234//53 + 5678//84)
+
+# 4th program
+print(13.42, 42.13)
+print(type(13.42 and 42.13))
+print(13 == 13 or 42 == 42)
